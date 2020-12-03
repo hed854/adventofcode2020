919f70a8f75c7606c943bdbb7cb05158942c6d7b
hed854 <hed854@users.noreply.github.com>
2020-12-03 23:38:13 +0100

    Solved day 3, part 1 with bonus display

--- day3/day3.py
+++ day3/day3.py
@@ -4,19 +4,72 @@ Advent of Code 2020 Day 3: Toboggan Trajectory
 https://adventofcode.com/2020/day/3
 """
 
+class ColoredString():
+    def red(self, text):
+        red = "\033[0;31m"
+        reset = "\033[0m"
+        return(f"{red}{text}{reset}")
+
+
 class Map():
+    """
+    Map object
+    - coordinates start on top left
+    - is structured in layers
+        * base_layer: the basic input map
+        * display_layer: for any modifications
+    """
     def __init__(self, input_file):
         with open(input_file) as f:
-            self.partial_data = f.readlines()
-        self.length = len(self.partial_data)
-        self.width = len(self.partial_data[0])
-
-    def expand(self):
-        """
-        Expand the basic map
-        """
-        pass
+            # Since these are list of strings, it's important we remove the pesky newline
+            self.base_layer = [line.rstrip("\n") for line in f]
+        self.height = len(self.base_layer)
+        self.width = len(self.base_layer[0])
+        # create a new modifiable map layer (base_layer strings are immutable)
+        # note: creating the empty list like this:
+        #   self.display_layer = [[None]*self.width]*self.height]
+        # creates multiple references of itself instead of copies -_-
+        self.display_layer = [[None for x in range(self.width)] for x in range(self.height)]
 
     def get_tile(self, x, y):
         # wrap the map width
-        return self.partial_data[y][x%self.width]
+        return self.base_layer[y][x%self.width]
+
+    def set_tile(self, x, y, value):
+        # same, but for the display layer
+        self.display_layer[y][x%self.width] = value
+
+    def display(self):
+        c = ColoredString()
+        for i in range(self.height):
+            for j in range(self.width):
+                if self.display_layer[i][j] is not None:
+                    print(c.red(self.display_layer[i][j]), end="")
+                else:
+                    print(self.base_layer[i][j], end="")
+            print()
+
+class TobogganDrive():
+    """
+    """
+    def __init__(self, map):
+        self.map = map
+        self.start_position = [0,0]
+        self.run_vector = [3,1]
+        self.crashed_in_tree = 0
+
+    def run(self):
+        position = self.start_position
+        # Go to the bottom of the map
+        while position[1] < self.map.height:
+            tile = self.map.get_tile(position[0], position[1])
+            if tile == "#":
+                self.crashed_in_tree = self.crashed_in_tree + 1
+                self.map.set_tile(position[0], position[1], "X")
+            else:
+                self.map.set_tile(position[0], position[1], "0")
+            position = [position[0] + self.run_vector[0], position[1] + self.run_vector[1]]
+        return self.crashed_in_tree
+
+toboggan = TobogganDrive(Map("input"))
+print(toboggan.run())
--- day3/test_day3.py
+++ day3/test_day3.py
@@ -1,5 +1,11 @@
 import pytest
-from day3 import Map
+import sys
+from day3 import Map, TobogganDrive
+
+def test_map_init():
+    t = Map("test_input")
+    assert 11 == t.height
+    assert 11 == t.width
 
 def test_map_coordinates():
     t = Map("test_input")
@@ -10,6 +16,29 @@ def test_map_coordinates():
     assert "." == t.get_tile(3,3)
     # wrapped coordinates
     assert "." == t.get_tile(12,0)
-    assert "." == t.get_tile(14,0)
-    assert "." == t.get_tile(12,0)
-    assert "." == t.get_tile(12,0)
+    assert "#" == t.get_tile(14,0)
+    assert "." == t.get_tile(12,3)
+    assert "." == t.get_tile(14,3)
+
+def test_map_display(capsys):
+    # Basic layer only
+    t = Map("test_input")
+    t.display()
+    capture = capsys.readouterr()
+    reference = "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#\n"
+    assert reference == capture.out
+    # With display layer and ANSI coloring
+    t.display_layer[0][0] = "X"
+    t.display()
+    capture = capsys.readouterr()
+    reference = "\033[0;31mX\033[0m.##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#\n"
+    assert reference == capture.out
+
+def test_drive(capsys):
+    t = TobogganDrive(Map("test_input"))
+    crashed = t.run()
+    assert 7 == crashed
+    t.map.display()
+    capture = capsys.readouterr()
+    reference = "\033[0;31m0\033[0m.##.......\n#..\033[0;31m0\033[0m#...#..\n.#....\033[0;31mX\033[0m..#.\n..#.#...#\033[0;31m0\033[0m#\n.\033[0;31mX\033[0m...##..#.\n..#.\033[0;31mX\033[0m#.....\n.#.#.#.\033[0;31m0\033[0m..#\n.#........\033[0;31mX\033[0m\n#.\033[0;31mX\033[0m#...#...\n#...#\033[0;31mX\033[0m....#\n.#..#...\033[0;31mX\033[0m.#\n"
+    assert reference == capture.out
