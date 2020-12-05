8d9eb31f0e153c16defaff3c3dabb36dfe41da72
hed854 <hed854@users.noreply.github.com>
2020-12-05 12:27:48 +0100

    Solved day 4, part 2

--- day4/day4.py
+++ day4/day4.py
@@ -9,6 +9,7 @@ class Passport():
         self.data = data
         self.fields = self.parse()
 
+
     def parse(self):
         parsed_dict = {}
         for i in self.data.split():
@@ -16,13 +17,109 @@ class Passport():
             parsed_dict[pair[0]] = pair[1]
         return parsed_dict
 
+
     def validate(self):
+        """
+        2 step validation
+        """
+        if self.global_validate():
+            return self.minute_validate()
+        else:
+            return False
+
+
+    def global_validate(self):
+        """
+        Field count and optional fields
+        """
         if len(self.fields) == 8:
             return True
         elif 'cid' not in self.fields and len(self.fields) == 7:
             return True
         else:
             return False
+
+
+    def minute_validate(self):
+        """
+        Validate each field precisely
+        """
+        validation_result = []
+        for k in self.fields:
+            if k == "byr":
+                validation_result.append(self.year_validate(k, 4, 1920, 2002))
+            elif k == "iyr":
+                validation_result.append(self.year_validate(k, 4, 2010, 2020))
+            elif k == "eyr":
+                validation_result.append(self.year_validate(k, 4, 2020, 2030))
+            elif k == "hgt":
+                validation_result.append(self.height_validate(k))
+            elif k == "hcl":
+                validation_result.append(self.haircolor_validate(k))
+            elif k == "ecl":
+                validation_result.append(self.eyecolor_validate(k))
+            elif k == "pid":
+                validation_result.append(self.id_validate(k))
+            else:
+                # CID case
+                pass
+        if False in validation_result:
+            return False
+        else:
+            return True
+
+    def year_validate(self, field_name, length, lower_bound, upper_bound):
+        if len(self.fields[field_name]) == length and int(self.fields[field_name]) >= lower_bound and int(self.fields[field_name]) <= upper_bound:
+            return True
+        else:
+            return False
+
+
+    def height_validate(self, field_name):
+        try:
+            unit = self.fields[field_name][-2:]
+            if unit not in ["cm", "in"]:
+                return False
+
+            value = int(self.fields[field_name][0:-2])
+
+            if unit == "cm" and value >= 150 and value <= 193:
+                return True
+
+            if unit == "in" and value >= 59 and value <= 76:
+                return True
+
+            return False
+        except:
+            print("unit:", unit)
+            print("value:", value)
+
+
+    def haircolor_validate(self, field_name):
+        if self.fields[field_name][0] != "#":
+            return False
+        value = self.fields[field_name][1:]
+        # Try to convert the hex into decimal to validate
+        try:
+            int(f"0x{value}", 16)
+        except:
+            return False
+
+        return True
+
+
+    def eyecolor_validate(self, field_name):
+        if not self.fields[field_name] in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
+            return False
+        else:
+            return True
+
+
+    def id_validate(self, field_name):
+        if len(self.fields[field_name]) != 9:
+            return False
+        else:
+            return True
 
 
 def batch_import(input_file):
@@ -43,5 +140,10 @@ def batch_import(input_file):
         batch.append(" ".join(a))
         return batch
 
+# Part 1: global validation
+#passports_validation = [Passport(i).global_validate() for i in batch_import("input")]
+#print(passports_validation.count(True))
+
+# Part 2: full validation (global + minute)
 passports_validation = [Passport(i).validate() for i in batch_import("input")]
 print(passports_validation.count(True))
--- day4/test_day4.py
+++ day4/test_day4.py
@@ -12,16 +12,79 @@ def test_passport_parse():
     reference = {"ecl": "gry",  "pid": "860033327", "eyr":"2020", "hcl":"#fffffd", "byr":"1937", "iyr":"2017", "cid":"147", "hgt":"183cm"}
     assert reference == p.parse()
 
-def test_passport_validate():
+def test_passport_global_validate():
     p = Passport("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm")
-    assert True == p.validate()
+    assert True == p.global_validate()
 
     p = Passport("iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929")
-    assert False == p.validate()
+    assert False == p.global_validate()
 
     p = Passport("hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm")
-    assert True == p.validate()
+    assert True == p.global_validate()
 
     p = Passport("hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in")
-    assert False == p.validate()
+    assert False == p.global_validate()
 
+
+def test_passport_year_validate():
+    p = Passport("byr:2002 iyr:2011 eyr:2022")
+    assert True == p.year_validate("byr", 4, 1920, 2002)
+    assert True == p.year_validate("iyr", 4, 2010, 2020)
+    assert True == p.year_validate("eyr", 4, 2020, 2030)
+
+    p = Passport("byr:1900 iyr:2001 eyr:2080")
+    assert False == p.year_validate("byr", 4, 1920, 2002)
+    assert False == p.year_validate("iyr", 4, 2010, 2020)
+    assert False == p.year_validate("eyr", 4, 2020, 2030)
+
+
+def test_passport_height_validate():
+    p = Passport("hgt:60in")
+    assert True == p.height_validate("hgt")
+    p = Passport("hgt:190cm")
+    assert True == p.height_validate("hgt")
+    p = Passport("hgt:190in")
+    assert False == p.height_validate("hgt")
+    p = Passport("hgt:190")
+    assert False == p.height_validate("hgt")
+
+
+def test_passport_haircolor_validate():
+    p = Passport("hcl:#123abc")
+    assert True == p.haircolor_validate("hcl")
+    p = Passport("hcl:#123abz")
+    assert False == p.haircolor_validate("hcl")
+    p = Passport("hcl:123abz")
+    assert False == p.haircolor_validate("hcl")
+
+
+def test_passport_eyecolor_validate():
+    p = Passport("ecl:brn")
+    assert True == p.eyecolor_validate("ecl")
+    p = Passport("ecl:wat")
+    assert False == p.eyecolor_validate("ecl")
+
+
+def test_passport_id_validate():
+    p = Passport("pid:000000001")
+    assert True == p.id_validate("pid")
+    p = Passport("pid:0123456789")
+    assert False == p.id_validate("pid")
+
+def test_passport_minute_validate():
+    p = Passport("eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926")
+    assert False == p.minute_validate()
+    p = Passport("iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946")
+    assert False == p.minute_validate()
+    p = Passport("hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277")
+    assert False == p.minute_validate()
+    p = Passport("hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007")
+    assert False == p.minute_validate()
+    p = Passport("pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f")
+    assert True == p.minute_validate()
+    p = Passport("eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm")
+    assert True == p.minute_validate()
+    p = Passport("hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022")
+    assert True == p.minute_validate()
+    p = Passport("iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719")
+    assert True == p.minute_validate()
